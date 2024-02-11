6063eadf6301fb9ee7b1d552c967f271ccc2942c
NAO <nao2bs@gmail.com>
2024-02-12 07:33:10 +0900

    docker compose add setting init: true/rails_admin 仮のダッシュボード

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,14 @@ class Post < ApplicationRecord
     has_many :comments
 
     def self.ransackable_attributes(auth_object = nil)
-        ["author", "content", "created_at", "date", "id", "title", "updated_at"]
+      ["author", "content", "created_at", "date", "id", "title", "updated_at"]
     end
 
     def self.ransackable_associations(auth_object = nil)
-        ["comments"]
-      end
+      ["comments"]
+    end
+
+    def self.recent(limit)
+      order(created_at: :desc).limit(limit)
+    end
 end
