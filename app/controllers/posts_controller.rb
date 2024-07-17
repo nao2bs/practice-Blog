247cfaabb23a9daa4774b569aa0f01f12f9702cc
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-07-17 09:55:58 +0000

    commit RailsAdmin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,11 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.all.page(params[:page]).per(2)
+    @posts = Post.all.page(params[:page]).per(5)
   end
 
   def show
+    @posts = Post.all.page(params[:page])
   end
 
   private
