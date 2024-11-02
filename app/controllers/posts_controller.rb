9887fa9fabbfaf8179298ec77c7d3ca5d5846cff
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-11-02 07:23:11 +0000

    kamanariを設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.all.page(params[:page]).per(5)
+    @posts = Post.all.page(params[:page])
+    # @posts = Post.all.page(params[:page]).per(5)
   end
 
   def show
@@ -10,6 +11,7 @@ class PostsController < ApplicationController
   end
 
   private
+
   def set_post
     @post = Post.find(params[:id])
   end
