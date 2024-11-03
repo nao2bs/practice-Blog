1131365623beaae14e230686a0cf9b69706d6aa6
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-11-03 07:21:21 +0000

    個別ページのページネーションを設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,20 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.all.page(params[:page])
+    @posts = Post.all.page(params[:page]).per(5)
     # @posts = Post.all.page(params[:page]).per(5)
   end
 
+  # def show
+  #   @posts = Post.all.page(params[:page])
+
   def show
-    @posts = Post.all.page(params[:page])
+    @post = Post.find(params[:id])
+
+    @prev_post = Post.where('id < ?', @post.id).order(id: :desc).first
+    @next_post = Post.where('id > ?', @post.id).order(id: :asc).first
   end
+  # end
 
   private
 
